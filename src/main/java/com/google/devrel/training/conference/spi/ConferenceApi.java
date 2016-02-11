98b540527168174189b41fed2f7a167435adbc02
fakeburst <lolguy322@gmail.com>
2016-02-11 13:34:19 +0200

    Profile functions complited

--- src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -16,109 +16,118 @@ import com.googlecode.objectify.Key;
 /**
  * Defines conference APIs.
  */
-@Api(name = "conference", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = {
-        Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, description = "API for the Conference Central Backend application.")
+@Api(name = "conference", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = { Constants.WEB_CLIENT_ID,
+		Constants.API_EXPLORER_CLIENT_ID }, description = "API for the Conference Central Backend application.")
 public class ConferenceApi {
 
-    /*
-     * Get the display name from the user's email. For example, if the email is
-     * lemoncake@example.com, then the display name becomes "lemoncake."
-     */
-    private static String extractDefaultDisplayNameFromEmail(String email) {
-        return email == null ? null : email.substring(0, email.indexOf("@"));
-    }
+	/*
+	 * Get the display name from the user's email. For example, if the email is
+	 * lemoncake@example.com, then the display name becomes "lemoncake."
+	 */
+	private static String extractDefaultDisplayNameFromEmail(String email) {
+		return email == null ? null : email.substring(0, email.indexOf("@"));
+	}
 
-    /**
-     * Creates or updates a Profile object associated with the given user
-     * object.
-     *
-     * @param user
-     *            A User object injected by the cloud endpoints.
-     * @param profileForm
-     *            A ProfileForm object sent from the client form.
-     * @return Profile object just created.
-     * @throws UnauthorizedException
-     *             when the User object is null.
-     */
+	/**
+	 * Creates or updates a Profile object associated with the given user
+	 * object.
+	 *
+	 * @param user
+	 *            A User object injected by the cloud endpoints.
+	 * @param profileForm
+	 *            A ProfileForm object sent from the client form.
+	 * @return Profile object just created.
+	 * @throws UnauthorizedException
+	 *             when the User object is null.
+	 */
 
-    // Declare this method as a method available externally through Endpoints
-    @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
-    // The request that invokes this method should provide data that
-    // conforms to the fields defined in ProfileForm
+	// Declare this method as a method available externally through Endpoints
+	@ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
+	// The request that invokes this method should provide data that
+	// conforms to the fields defined in ProfileForm
 
-    // TODO 1 Pass the ProfileForm parameter
-    // TODO 2 Pass the User parameter
-    public Profile saveProfile(ProfileForm form, User u) throws UnauthorizedException {
+	// TODO 1 Pass the ProfileForm parameter
+	// TODO 2 Pass the User parameter
+	public Profile saveProfile(ProfileForm form, User u) throws UnauthorizedException {
 
-        String userId = null;
-        String mainEmail = null;
-        String displayName = "Your name will go here";
-        TeeShirtSize teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
+		String userId = null;
+		String mainEmail = null;
+		String displayName = "Your name will go here";
+		TeeShirtSize teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
 
-        // TODO 2
-        // If the user is not logged in, throw an UnauthorizedException
-        if(u == null)
-        	throw new UnauthorizedException("Please login.");
+		// TODO 2
+		// If the user is not logged in, throw an UnauthorizedException
+		if (u == null)
+			throw new UnauthorizedException("Please login.");
 
-        // TODO 1
-        // Set the teeShirtSize to the value sent by the ProfileForm, if sent
-        // otherwise leave it as the default value
-        
-        if(form.getTeeShirtSize()!= null)
-        	teeShirtSize = form.getTeeShirtSize();
+		// TODO 1
+		// Set the teeShirtSize to the value sent by the ProfileForm, if sent
+		// otherwise leave it as the default value
 
-        // TODO 1
-        // Set the displayName to the value sent by the ProfileForm, if sent
-        // otherwise set it to null
+		if (form.getTeeShirtSize() != null)
+			teeShirtSize = form.getTeeShirtSize();
 
-        if(form.getDisplayName() != null)
-        	displayName = form.getDisplayName();
-        
-        // TODO 2
-        // Get the userId and mainEmail
-        
-        userId = u.getUserId();
-        mainEmail = u .getEmail();
+		// TODO 1
+		// Set the displayName to the value sent by the ProfileForm, if sent
+		// otherwise set it to null
 
-        // TODO 2
-        // If the displayName is null, set it to default value based on the user's email
-        // by calling extractDefaultDisplayNameFromEmail(...)
+		if (form.getDisplayName() != null)
+			displayName = form.getDisplayName();
 
-        if(displayName == null)
-        	displayName = extractDefaultDisplayNameFromEmail(mainEmail);
-        
-        // Create a new Profile entity from the
-        // userId, displayName, mainEmail and teeShirtSize
-        Profile profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
+		// TODO 2
+		// Get the userId and mainEmail
 
-        // TODO 3 (In Lesson 3)
-        // Save the Profile entity in the datastore
+		userId = u.getUserId();
+		mainEmail = u.getEmail();
 
-        // Return the profile
-        return profile;
-    }
+		// TODO 2
+		// If the displayName is null, set it to default value based on the
+		// user's email
+		// by calling extractDefaultDisplayNameFromEmail(...)
 
-    /**
-     * Returns a Profile object associated with the given user object. The cloud
-     * endpoints system automatically inject the User object.
-     *
-     * @param user
-     *            A User object injected by the cloud endpoints.
-     * @return Profile object.
-     * @throws UnauthorizedException
-     *             when the User object is null.
-     */
-    @ApiMethod(name = "getProfile", path = "profile", httpMethod = HttpMethod.GET)
-    public Profile getProfile(final User user) throws UnauthorizedException {
-        if (user == null) {
-            throw new UnauthorizedException("Authorization required");
-        }
+		if (displayName == null)
+			displayName = extractDefaultDisplayNameFromEmail(mainEmail);
 
-        // TODO
-        // load the Profile Entity
-        String userId = ""; // TODO
-        Key key = null; // TODO
-        Profile profile = null; // TODO load the Profile entity
-        return profile;
-    }
+		// Create a new Profile entity from the
+		// userId, displayName, mainEmail and teeShirtSize
+		Profile profile = getProfile(u);
+		if (profile == null)
+			profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
+		else
+			profile.update(displayName, teeShirtSize);
+
+		// TODO 3 (In Lesson 3)
+		// Save the Profile entity in the datastore
+		ofy().save().entity(profile).now();
+
+		// Return the profile
+		return profile;
+	}
+
+	/**
+	 * Returns a Profile object associated with the given user object. The cloud
+	 * endpoints system automatically inject the User object.
+	 *
+	 * @param user
+	 *            A User object injected by the cloud endpoints.
+	 * @return Profile object.
+	 * @throws UnauthorizedException
+	 *             when the User object is null.
+	 */
+	@ApiMethod(name = "getProfile", path = "profile", httpMethod = HttpMethod.GET)
+	public Profile getProfile(final User user) throws UnauthorizedException {
+		if (user == null) {
+			throw new UnauthorizedException("Authorization required");
+		}
+
+		// TODO
+		// load the Profile Entity
+		String userId = user.getUserId(); // TODO
+		Key key = Key.create(Profile.class, userId); // TODO
+		Profile profile = (Profile) ofy().load().key(key).now(); // TODO load
+																	// the
+																	// Profile
+																	// entity
+		return profile;
+	}
 }
